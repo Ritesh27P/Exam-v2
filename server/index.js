8e04a33bdcf455b5831ccd7c8235402f00a7a13c
ritesh-blog27 <riteshminecraft27@gmail.com>
2023-04-27 14:14:46 +0530

    Exam-app

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+
+const PORT = process.env.PORT || 5000
+
+// configure
+const app = express();
+app.use(express.json())
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    })
+    .then(app.listen(PORT, ()=> {console.log('The server is running on localhost:' + PORT)}))
+    .catch(err => console.log(err))
